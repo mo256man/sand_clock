05e2a38ae39be760adda7becee7c5d4be5cc9dca
mo256man <48610559+mo256man@users.noreply.github.com>
2025-05-19 22:12:15 +0900

    Add files via upload

--- sand.py
+++ sand.py
@@ -29,11 +29,11 @@ class App():
         self.width = 800
         self.height = 640
         self.image = np.full((self.height, self.width, 3), WHITE, np.uint8)
-        self.error = [0, 0]  # 累積誤差を管理する変数
 
         # グリッド
-        self.division = 16
+        self.division = 32
         self.grid = np.zeros((self.division, self.division), dtype=int)
+        self.errors =  np.zeros((self.division, self.division, 2), dtype=float)     # 累積誤差を管理する変数
 
         # 円の外を壁にする
         columns = [range(5), range(3), range(2), [0], [0]]
@@ -76,31 +76,32 @@ class App():
 
     # 累積誤差補正法を使用した最も重力方向に近い隣接セルを計算
     def find_closest_cell_with_error(self, r, c):
-        dx, dy = self.gravity  # 重力ベクトルの成分
-        self.error[0] += dx  # x方向の累積誤差を更新
-        self.error[1] += dy  # y方向の累積誤差を更新
+        # dx, dy = self.gravity  # 重力ベクトルの成分
+        self.errors[r, c, :2] += self.gravity
+        error_x, error_y = self.errors[r, c]
 
         move_x, move_y = 0, 0
 
-        # 縦方向の移動を優先
-        if abs(self.error[1]) >= 1:
-            move_y = 1 if self.error[1] > 0 else -1
-            self.error[1] -= math.copysign(1, self.error[1])  # 誤差補正
+        if abs(error_y) >= 1:
+            move_y = 1 if error_y > 0 else -1
+            error_y -= math.copysign(1, error_y)  # 誤差補正
 
-        # 横方向の移動を次に考慮
-        if abs(self.error[0]) >= 1:
-            move_x = 1 if self.error[0] > 0 else -1
-            self.error[0] -= math.copysign(1, self.error[0])  # 誤差補正
+        if abs(error_x) >= 1:
+            move_x = 1 if error_x > 0 else -1
+            error_x -= math.copysign(1, error_x)  # 誤差補正
 
         # 移動先セルの決定
         nr, nc = r + move_y, c + move_x
         if 0 <= nr < self.division and 0 <= nc < self.division and self.grid[nr, nc] == 0:
+            self.errors[nr, nc] = (error_x, error_y)
+            self.errors[r, c] = (0, 0)
             return nr, nc
 
         # 移動が無効な場合はその場にとどまる
         return r, c
 
-
+    def reset_errors(self):
+        self.errors =  np.zeros((self.division, self.division, 2), dtype=float)
 
     def simulate_sand(self):
         self.gravity = gravity_vector(self.angle)
@@ -135,12 +136,25 @@ class App():
                 if color:
                     cv2.rectangle(disc, (x0,y0), (x1,y1), color, -1)
 
-        M = cv2.getRotationMatrix2D((self.disc_size//2, self.disc_size//2), self.angle, 1)
+        # 基準の真下
+        x0, y0 = self.disc_size//2, self.disc_size//2
+        x1, y1 = x0, self.disc_size
+        cv2.line(disc, (x0, y0), (x1, y1), RED, 1)
+
+        # 重力の方向
+        gx, gy = gravity_vector(self.angle)
+        x1 = int(x0 + self.disc_size * gx)
+        y1 = int(y0 + self.disc_size * gy)
+        cv2.line(disc, (x0, y0), (x1, y1), BLUE, 1)
+
+        M = cv2.getRotationMatrix2D((self.disc_size//2, self.disc_size//2), -self.angle, 1)
         disc_rot = cv2.warpAffine(disc, M, (self.disc_size, self.disc_size))
         x0 = (self.width - self.disc_size)//2
         x1 = (self.width + self.disc_size)//2
+        self.image[:, x0:x1] = disc
+        cv2.imshow("origin", self.image)
         self.image[:, x0:x1] = disc_rot
-        cv2.imshow("", self.image)
+        cv2.imshow("rot", self.image)
 
 def main():
     app = App()
@@ -152,8 +166,10 @@ def main():
             break
         elif key == ord("a"):
             app.angle += 10
+            app.reset_errors()
         elif key == ord("d"):
             app.angle -= 10
+            app.reset_errors()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
